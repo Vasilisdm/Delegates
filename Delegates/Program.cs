a995c7b9dcf69a37439c0ec7230e8367e751858c
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-22 22:06:58 +0200

    Added WorkPerformedHandler delegate.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,11 +2,14 @@
 
 namespace Delegates
 {
+
+    public delegate void WorkPerformedHandled(int hours, WorkType workType);
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
