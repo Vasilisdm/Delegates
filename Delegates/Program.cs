24d054ba95a7d3edb319e986c223fa05b7ad8b66
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-22 22:26:38 +0200

    Created 2 methods with same signature as the delegate, initialized 2 instances of the delegate, created a static method wich takes a delegate, and finally called that method.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -3,13 +3,31 @@
 namespace Delegates
 {
 
-    public delegate void WorkPerformedHandled(int hours, WorkType workType);
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            DoWork(del1);
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.FixBugs);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed1 Hours worked: {hours}, Work type: {workType}");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed2 Hours worked: {hours}, Work type: {workType}");
         }
     }
 }
