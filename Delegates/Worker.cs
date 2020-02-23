32f06eac6cb36ec9696a371bd0e162bbac6cde8d
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-23 20:37:53 +0200

    Created event raiser methods: OnWorkPerformed and OnWorkCompleted and called those 2 inside the do work method.

--- Delegates/Worker.cs
+++ Delegates/Worker.cs
@@ -10,7 +10,27 @@ namespace Delegates
 
         public void DoWork(int hours, WorkType workType)
         {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i + 1, WorkType.WriteCode);
+            }
+            OnWorkedCompleted();
+        }
 
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            if (WorkPerformed is WorkPerformedHandler del)
+            {
+                del(hours, workType);
+            }
+        }
+
+        protected virtual void OnWorkedCompleted()
+        {
+            if (WorkCompleted is EventHandler del)
+            {
+                del(this, EventArgs.Empty);
+            }
         }
     }
 }
