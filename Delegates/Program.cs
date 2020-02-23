13ac234871258d6dfe8dc2bc357a77d825a591b8
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-23 21:53:29 +0200

    Added Worker_WorkPerformed to delegate's invocation list and executed the DoWork method.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -7,6 +7,13 @@ namespace Delegates
         static void Main(string[] args)
         {
             Worker worker = new Worker();
+            worker.WorkPerformed += new WorkPerformedHandler(Worker_WorkPerformed);
+            worker.DoWork(8, WorkType.WriteCode);
+        }
+
+        static void Worker_WorkPerformed(object sender, WorkedPerformedEventArgs e)
+        {
+            Console.WriteLine($"Hours worked: {e.Hours}, Type of Work: {e.WorkType}");
         }
     }
 }
