3f1a7120dd4ef84ca1a505bdea9b428642124d31
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-26 00:00:34 +0200

    Added the process data class in which I use a method which accepts 2 integers and a delegate of type BizRulesDelegate.

---
+++ Delegates/ProcessData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Delegates
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine($"Result of the delegate process is: {result}");
+        }
+    }
+}
