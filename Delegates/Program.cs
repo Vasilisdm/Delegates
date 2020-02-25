e2443c2843cba6da95481f06ecda371427f920e7
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-25 23:22:30 +0200

    Used anonymous methods to hook event handler code at the event source.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -7,19 +7,28 @@ namespace Delegates
         static void Main(string[] args)
         {
             Worker worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkCompleted += Worker_WorkCompleted;
+
+            worker.WorkPerformed += delegate(object sender, WorkedPerformedEventArgs e)
+            {
+                Console.WriteLine($"Hours worked: {e.Hours}, Type of Work: {e.WorkType}");
+            };
+
+            worker.WorkCompleted += delegate (object sender, EventArgs e)
+            {
+                Console.WriteLine($"Work is done!");
+            };
+
             worker.DoWork(8, WorkType.CodeWriting);
         }
 
-        static void Worker_WorkPerformed(object sender, WorkedPerformedEventArgs e)
-        {
-            Console.WriteLine($"Hours worked: {e.Hours}, Type of Work: {e.WorkType}");
-        }
+        //static void Worker_WorkPerformed(object sender, WorkedPerformedEventArgs e)
+        //{
+        //    Console.WriteLine($"Hours worked: {e.Hours}, Type of Work: {e.WorkType}");
+        //}
 
-        static void Worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine($"Work is done!");
-        }
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine($"Work is done!");
+        //}
     }
 }
